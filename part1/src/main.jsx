92bb8d20ab3607eb215efdcd5c06a2b52a98197a
Iamrahul4u <iamrahulgupta4u@gmail.com>
2023-09-09 01:35:21 +0530

    my submission

---
+++ part1/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// UnComment Below Line to run Exercise from 1.6 to 1.11
+// import App from "./Unicafe";
+
+// UnComment Below Line to run Exercise from 1.11 to 1.14
+import App from "./Anecdotes";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
