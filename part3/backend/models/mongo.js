e5f47845712fe1c9f6d0606ef0d6563540caca9b
Iamrahul4u <iamrahulgupta4u@gmail.com>
2023-09-12 20:07:48 +0530

    mongo db integration

---
+++ part3/backend/models/mongo.js
@@ -0,0 +1,67 @@
+// const mongoose = require("mongoose");
+
+// if (process.argv.length < 3) {
+//   console.log("give password as argument");
+//   process.exit(1);
+// }
+
+// const password = process.argv[2];
+// const name = process.argv[3];
+// const number = process.argv[4];
+// const url = `mongodb+srv://iamrahulgupta4u:${password}@cluster0.byvahhi.mongodb.net/Person?retryWrites=true&w=majority`;
+// mongoose.set("strictQuery", false);
+// mongoose.connect(url);
+
+// const noteSchema = new mongoose.Schema({
+//   name: String,
+//   number: Number,
+// });
+
+// const Person = mongoose.model("person", noteSchema);
+
+// if (process.argv.length < 4) {
+//   Person.find({}).then((persons) => {
+//     console.log("phonebook:");
+//     persons.forEach((person) => {
+//       console.log(person.name, person.number);
+//     });
+//   });
+// } else {
+//   const person = new Person({
+//     name: name,
+//     number: number,
+//   });
+//   person.save().then(() => {
+//     console.log("note saved!");
+//     mongoose.connection.close();
+//   });
+// }
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const url = process.env.MONGODB_URI;
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const phoneSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+});
+
+phoneSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Person", phoneSchema);
--- part3/backend/persons.js
+++ part3/backend/persons.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
+const Phone = require("./models/mongo");
 var morgan = require("morgan");
 const cors = require("cors");
-
 let persons = [
   {
     id: 1,
@@ -29,30 +29,29 @@ app.use(express.static("dist"));
 app.use(cors());
 app.use(morgan("tiny"));
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Phone.find({}).then((person) => {
+    response.json(person);
+  });
 });
 
 app.get("/info", (request, response) => {
   const currentTime = new Date();
-  const info = `<p>Phone has info for ${persons.length} people</p>
+  const info = `<p>Phone has info for ${Phone.length} people</p>
     <p>${currentTime.toString()}</p>
   `;
   response.send(info);
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const person = persons.find(
-    (person) => person.id === Number(request.params.id)
-  );
-  if (!person) {
-    return response.status(404).end();
-  } else response.json(person);
+  Phone.findById(request.params.id).then((person) => {
+    response.json(person);
+  });
+  // if (!person) {
+  //   return response.status(404).end();
+  // } else response.json(person);
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  persons = persons.filter((person) => person.id !== Number(request.params.id));
-  response.status(204).end();
-});
+app.delete("/api/persons/:id", (request, response) => {});
 
 const generateRandom = () => {
   const id = Math.floor(Math.random() * 10000000000000000000000);
@@ -62,22 +61,22 @@ app.use(express.json());
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
-  console.log(body);
-  const isExist = persons.some(
-    (person) => person.name.toLowerCase() === body.name.toLowerCase()
-  );
-  if (!body.name || !body.number) {
-    return response.status(400).json({ error: "Content is Missing" });
-  } else if (isExist) {
-    return response.status(400).json({ error: "Name already exists" });
-  }
-  const person = {
+  // const isExist = persons.some(
+  //   (person) => person.name.toLowerCase() === body.name.toLowerCase()
+  // );
+  // if (!body.name || !body.number) {
+  //   return response.status(400).json({ error: "Content is Missing" });
+  // } else if (isExist) {
+  //   return response.status(400).json({ error: "Name already exists" });
+  // }
+  const person = new Phone({
     name: body.name,
     number: body.number,
     id: generateRandom(), //generate a random id
-  };
-  persons = persons.concat(person);
-  response.json(person);
+  });
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 const PORT = process.env.VITE_PORT || 3001;
 app.listen(PORT, () => {
