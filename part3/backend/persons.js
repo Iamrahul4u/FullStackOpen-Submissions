49b8e52536a9d0f1797b5f52c2fe47fee00e6097
Iamrahul4u <iamrahulgupta4u@gmail.com>
2023-09-13 10:45:13 +0530

    Exercise 3.18 submit

--- part3/backend/persons.js
+++ part3/backend/persons.js
@@ -23,18 +23,21 @@ app.get("/info", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
-  // if (!person) {
-  //   return response.status(404).end();
-  // } else response.json(person);
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response) => {
   Person.findByIdAndRemove(request.params.id)
-    .then((result) => {
+    .then((person) => {
       response.status(204).end();
     })
     .catch((error) => next(error));
@@ -66,7 +69,7 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
-app.put("/api/persons/:id", (request, response) => {
+app.put("/api/persons/:id", (request, response, next) => {
   const body = request.body;
   const person = new Person({
     id: request.params.id,
@@ -77,8 +80,19 @@ app.put("/api/persons/:id", (request, response) => {
     .then((updatedNote) => {
       response.json(updatedNote);
     })
-    .catch((error) => console.log(error.message));
+    .catch((error) => next(error));
 });
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 const PORT = process.env.VITE_PORT || 3001;
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
